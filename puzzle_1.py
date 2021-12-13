aab1425872ab5da60f8d3191d17e1f09b1843b41
CarlRazmus <carl.razmus@outlook.com>
2021-12-13 22:07:17 +0100

    cleanup puzzle_1

--- puzzle_1.py
+++ puzzle_1.py
@@ -1,29 +1,13 @@
 def part_1():
-    lines = open("input.txt")
-    numbers = [int(n.strip()) for n in lines]
-    c = 0 
-    prev_number = 99999999999999999999
-    for number in numbers:
-        if number > prev_number:
-            c = c + 1
-        prev_number = number
-    print(c)
+    print(len([n for idx, n in enumerate(numbers[1:]) if n > numbers[idx]]))
+
 
 def part_2():
-    lines = open("input.txt")
-    numbers = [int(n.strip()) for n in lines]
-    c = 0 
-    prev_value = 99999999999999999999
-    for idx, number in enumerate(numbers):
-        if idx + 3 <= len(numbers):
-            value = sum(numbers[idx : idx+3])
-            print(numbers[idx : idx+3])
-            if value > prev_value:
-                c = c + 1
-            prev_value = value
-    print(c)
+    print(len([idx for idx in range(len(numbers)-2) if sum(numbers[idx+1:idx+4]) > sum(numbers[idx:idx+3])]))
 
 
 if __name__ == "__main__":
-    #part_1()
+    with open("input_1.txt") as f:
+        numbers = [int(n.strip()) for n in f]
+    part_1()
     part_2()
