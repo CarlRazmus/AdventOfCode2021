a34402c8197f415075e299c9ef82d8c22aa3f9c1
CarlRazmus <carl.razmus@outlook.com>
2021-12-13 22:24:59 +0100

    cleanup puzzle 3

--- puzzle_3_remake.py
+++ puzzle_3_remake.py
@@ -1,7 +1,9 @@
 import operator
 
+
 def getMostOrLeastCommonNr(l, op):
     return 0 if op(l.count(0), l.count(1)) else 1
+
 
 def calc_rating(input_matrix, op, idx):
     if len(input_matrix) == 1:
@@ -11,19 +13,21 @@ def calc_rating(input_matrix, op, idx):
     new_matrix = [l for l in input_matrix if most_or_least_common_nr == l[idx]]
     return calc_rating(new_matrix, op, idx + 1)
 
+
 def part1():
-    input_matrix = [[int(n) for n in l] for l in [line.strip() for line in open("input.txt")]]
     flipped_input_matrix = [list(i) for i in zip(*input_matrix)]
     gamma_rate = "".join([str(getMostOrLeastCommonNr(l, operator.gt)) for l in flipped_input_matrix])
     epsilon = "".join([str(getMostOrLeastCommonNr(l, operator.lt)) for l in flipped_input_matrix])
     print(int(str(gamma_rate), 2) * int(str(epsilon), 2))
 
+
 def part2():
-    input_matrix = [[int(n) for n in l] for l in [line.strip() for line in open("input.txt")]]
     oxygen_rate = int("".join(str(x) for x in calc_rating(input_matrix, operator.gt, 0)), 2)
     other_rate = int("".join(str(x) for x in calc_rating(input_matrix, operator.le, 0)), 2)
     print(oxygen_rate * other_rate)
 
+
 if __name__ == "__main__":
+    input_matrix = [[int(n) for n in l] for l in [line.strip() for line in open("input_3.txt")]]
     part1()
     part2()
